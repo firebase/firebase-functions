284fecd2ab5d3e4afeef753c91710ef62d5117c3
Thomas Bouldin <thomas.bouldin@gmail.com>
2016-10-07 17:46:46 -0700

    Try to set up Env in parallel with requires. (#45)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import Apps from './apps';
+// The Firebase Functions runtime is currently a bit slow at resolving requires. Make sure we
+// kick off the fetch for Credential and RuntimeConfig while it's also fetching other
+// dependencies.
 import { ApplicationDefaultCredential } from './credential';
-import FirebaseFunctions from './functions';
-import { RuntimeConfigEnv } from './env';
-
 const credential = new ApplicationDefaultCredential();
+credential.getAccessToken();
+
+import { RuntimeConfigEnv } from './env';
 const env = new RuntimeConfigEnv(credential, process.env.GCLOUD_PROJECT);
+
+import Apps from './apps';
+import FirebaseFunctions from './functions';
+
 const apps = new Apps(env);
 
 const functionsApi: FirebaseFunctions = new FirebaseFunctions(env, apps);
