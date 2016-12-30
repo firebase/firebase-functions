bea4e13652760211bad096de01e35ee1088b992d
Lauren Long <longlauren@google.com>
2016-12-30 15:52:00 -0800

    Revert "add retain, release, and ref counters"
    This reverts commit db51c765e8af98f68f44f5bba51784a2bf403e5d.

--- src/apps.ts
+++ src/apps.ts
@@ -1,136 +1,32 @@
 import * as _ from 'lodash';
 import * as firebase from 'firebase';
 import { FirebaseEnv } from './env';
-import * as Promise from 'bluebird';
 
 export interface AuthMode {
   admin: boolean;
   variable?: any;
 }
 
-export interface RefCounter {
-  admin: number;
-  noauth: number;
-  user: {[uuid: string]: number};
-}
-
 export default class Apps {
   private static _noauth: firebase.app.App;
   private static _admin: firebase.app.App;
+
   private _env: FirebaseEnv;
-  private _refCounter: RefCounter;
 
   constructor(env: FirebaseEnv) {
     this._env = env;
-    this._refCounter = {
-      admin: 0,
-      noauth: 0,
-      user: {},
-    };
   }
 
-  _waitToDestroyApp(app: firebase.app.App) {
-    if (!app) {
-      console.log('resolved promise since app is empty');
-      return Promise.resolve();
-    }
-    return Promise.delay(120000).then(() => {
-      console.log('deleting app');
-      return app.delete();
-    }).then(() => {
-      console.log('app deleted');
-    });
-  }
-
-  _changeRefCounter() {
-    return Promise.delay(120000).then(() => {
-      console.log('time passed');
-      this._refCounter = {
-        admin: 1000,
-        noauth: 1000,
-        user: {},
-      };
-    });
-  }
-
-  _waitToDestroyUserApp(key: string) {
-    try {
-      let app = firebase.app(key);
-      console.log('deleting user app...');
-      return this._waitToDestroyApp(app);
-    } catch (e) {
-      return Promise.resolve();
-    }
-  }
-
-  _waitToDestroyAdmin() {
-    console.log('deleting admin app...');
-    return this._waitToDestroyApp(Apps._admin);
-  }
-
-  _waitToDestroyNoauth() {
-    console.log('deleting noauth app...');
-    return this._waitToDestroyApp(Apps._noauth);
-  }
-
-  retain(payload) {
-    let auth: AuthMode | null = _.get(payload, 'auth', null);
-    this._refCounter.admin ++;
-    if (!auth || typeof auth !== 'object') {
-      this._refCounter.noauth ++;
-    } else if (auth.admin) {
-      this._refCounter.admin ++;
-    } else if (!auth.variable) {
-      this._refCounter.noauth ++;
-    } else {
-      const key = JSON.stringify(auth.variable);
-      let count = _.get(this._refCounter.user, key, 0);
-      _.set(this._refCounter.user, key, count+1);
-    }
-  }
-
-  release(payload) {
-    let auth: AuthMode | null = _.get(payload, 'auth', null);
-    this._refCounter.admin --;
-    if (!auth || typeof auth !== 'object') {
-      this._refCounter.noauth --;
-    } else if (auth.admin) {
-      this._refCounter.admin --;
-    } else if (!auth.variable) {
-      this._refCounter.noauth --;
-    } else {
-      const key = JSON.stringify(auth.variable);
-      let count = _.get(this._refCounter.user, key, 0);
-      if (count < 2) {
-        this._waitToDestroyUserApp(key);
-      }
-      _.set(this._refCounter.user, key, count-1);
-    }
-    if (this._refCounter.admin === 0) {
-      this._waitToDestroyAdmin();
-    }
-    if (this._refCounter.noauth === 0) {
-      this._waitToDestroyNoauth();
-    }
-  }
   get admin(): firebase.app.App {
-    try {
-      return firebase.app('__admin__');
-    } catch (e) {
-      console.log('Need to init admin app');
-      Apps._admin = firebase.initializeApp(this.firebaseArgs, '__admin__');
-      return Apps._admin;
-    }
+    // TODO(inlined) add credential to env
+    Apps._admin = Apps._admin || firebase.initializeApp(this.firebaseArgs, '__admin__');
+    return Apps._admin;
   }
 
   get noauth(): firebase.app.App {
-    try {
-      return firebase.app('__noauth__');
-    } catch (e) {
-      console.log('Need to init noauth app');
-      Apps._noauth = firebase.initializeApp(_.omit(this.firebaseArgs, 'credential'), '__noauth__');
-      return Apps._noauth;
-    }
+    Apps._noauth = Apps._noauth ||
+                  firebase.initializeApp(_.omit(this.firebaseArgs, 'credential'), '__noauth__');
+    return Apps._noauth;
   }
 
   forMode(auth: AuthMode): firebase.app.App {
