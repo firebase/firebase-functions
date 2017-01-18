8a343dcdb9b1ecb7182a8dd8e13a4a3bb6bed751
Robert-Jan Huijsman <rjh@google.com>
2017-01-17 17:39:23 -0800

    Introduce the basic scaffolding for a testing-only entry point in the Firebase Functions SDK.
    The testing functions themselves live with the rest of the SDK's code, in src/. The src/testing.ts file is however also the m"main" for the testing/package.json, making it an access point reachable through the import "firebase-functions/testing".
    
    The advantages of this approach are that generated code is still produced in outdir lib/, while the import path for testing-only methods remains appealing.
    
    We stop packing the test/ directory (our internal unit tests) for distribution, to avoid user confusion between "test/" and "testing/".

---
+++ src/testing.ts
@@ -0,0 +1,7 @@
+// This file is an entry point into the testing-only functionality of the
+// Firebase Functions SDK.
+
+// TODO(rjh): provide actual testing functionality.
+export function whereAreTheBugs(): string {
+  return 'Klendathu';
+}
