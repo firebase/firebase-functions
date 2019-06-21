819a564e5334738fe2b5fa5a8b22391feea7e8c7
Natan Sągol <m@merlinnot.com>
2019-06-21 16:29:46 +0200

    Reformat

--- integration_test/functions/src/index.ts
+++ integration_test/functions/src/index.ts
@@ -49,9 +49,7 @@ function callScheduleTrigger(functionName: string, region: string) {
       {
         method: 'POST',
         host: 'cloudscheduler.googleapis.com',
-        path: `projects/${
-          firebaseConfig.projectId
-        }/locations/us-central1/jobs/firebase-schedule-${functionName}-${region}:run`,
+        path: `projects/${firebaseConfig.projectId}/locations/us-central1/jobs/firebase-schedule-${functionName}-${region}:run`,
         headers: {
           'Content-Type': 'application/json',
         },
@@ -196,9 +194,7 @@ export const integrationTests: any = functions
         resp
           .status(500)
           .send(
-            `FAIL - details at https://${
-              process.env.GCLOUD_PROJECT
-            }.firebaseio.com/testRuns/${testId}`
+            `FAIL - details at https://${process.env.GCLOUD_PROJECT}.firebaseio.com/testRuns/${testId}`
           );
       });
   });
