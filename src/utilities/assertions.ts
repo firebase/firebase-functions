c476b09cd064bfda7020567be9aec0f826b299e3
Natan Sągol <m@merlinnot.com>
2019-07-12 21:18:59 +0200

    Add a dot at the end of an error message

--- src/utilities/assertions.ts
+++ src/utilities/assertions.ts
@@ -3,11 +3,13 @@
  * strictness of both type checking and runtime.
  */
 
- /**
-  * Checks that the given value is of type `never` — the type that’s left after
-  * all other cases have been removed.
-  * @param x A value of type `never`.
-  */
+/**
+ * Checks that the given value is of type `never` — the type that’s left after
+ * all other cases have been removed.
+ * @param x A value of type `never`.
+ */
 export function assertNever(x: never): never {
-  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(x)}`);
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(x)}.`
+  );
 }
