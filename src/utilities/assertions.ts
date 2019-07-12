c7a8ba44cb531881f8aa72e49726ff6928576b51
Natan Sągol <m@merlinnot.com>
2019-07-12 21:18:18 +0200

    Fix exhaustiveness of error code switch in http status conversion

---
+++ src/utilities/assertions.ts
@@ -0,0 +1,13 @@
+/**
+ * @file Provides common assertion helpers which can be used to improve
+ * strictness of both type checking and runtime.
+ */
+
+ /**
+  * Checks that the given value is of type `never` — the type that’s left after
+  * all other cases have been removed.
+  * @param x A value of type `never`.
+  */
+export function assertNever(x: never): never {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(x)}`);
+}
