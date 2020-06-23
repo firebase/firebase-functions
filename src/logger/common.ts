f9b24e03a5b9da08866f990304f6623a4b909be3
egilmorez <egilmore@google.com>
2020-06-23 12:56:52 -0700

    Adding logger SDK to reference, with some edits. (#714)
    * Adding logger SDK to reference, with some edits.
    
    * Adding link to structured logging details per feedback.

--- src/logger/common.ts
+++ src/logger/common.ts
@@ -1,9 +1,11 @@
 // Determine if structured logs are supported (node >= 10). If something goes wrong,
 // assume no since unstructured is safer.
+/** @hidden */
 export const SUPPORTS_STRUCTURED_LOGS =
   parseInt(process.versions?.node?.split('.')?.[0] || '8', 10) >= 10;
 
 // Map LogSeverity types to their equivalent `console.*` method.
+/** @hidden */
 export const CONSOLE_SEVERITY: {
   [severity: string]: 'debug' | 'info' | 'warn' | 'error';
 } = {
@@ -18,6 +20,7 @@ export const CONSOLE_SEVERITY: {
 };
 
 // safely preserve unpatched console.* methods in case of compat require
+/** @hidden */
 export const UNPATCHED_CONSOLE = {
   debug: console.debug,
   info: console.info,
