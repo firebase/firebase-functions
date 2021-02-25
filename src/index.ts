0bb65599eba022112ef1bd048dac2d51e18698ec
Thomas Bouldin <inlined@google.com>
2021-02-25 12:51:10 -0800

    Remove crashlyitcs

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@
 // Providers:
 import * as analytics from './providers/analytics';
 import * as auth from './providers/auth';
-import * as crashlytics from './providers/crashlytics';
 import * as database from './providers/database';
 import * as firestore from './providers/firestore';
 import * as https from './providers/https';
@@ -43,7 +42,6 @@ export {
   analytics,
   app,
   auth,
-  crashlytics,
   database,
   firestore,
   handler,
