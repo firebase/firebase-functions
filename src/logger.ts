fafc14a1d3c94d90b8135481b40bf254fc4a4f55
Joe Hanley <joehanley@google.com>
2021-06-28 15:44:25 -0700

    Defensively JSON.pase+JSON.stringify inputs into removeCircular to prevent NPEs

--- src/logger.ts
+++ src/logger.ts
@@ -30,7 +30,12 @@ export interface LogEntry {
   [key: string]: any;
 }
 
-function removeCircular(obj: any, refs: any[] = []): any {
+function removeCircular(obj: any, refs:any[] = []): any {
+  const cleanedObj = JSON.parse(JSON.stringify(obj));
+  return removeCircularRecursive(cleanedObj, refs);
+}
+
+function removeCircularRecursive(obj: any, refs: any[] = []): any {
   if (typeof obj !== 'object' || !obj) {
     return obj;
   }
@@ -49,7 +54,7 @@ function removeCircular(obj: any, refs: any[] = []): any {
     if (refs.includes(obj[k])) {
       returnObj[k] = '[Circular]';
     } else {
-      returnObj[k] = removeCircular(obj[k], refs);
+      returnObj[k] = removeCircularRecursive(obj[k], refs);
     }
   }
   return returnObj;
