8e21f77a3d5022963f0d83adf6a97fb55fa3c95d
Cole Rogers <colerogers@users.noreply.github.com>
2022-01-31 16:28:11 -0500

    Generic interface for provider enhancement (#1020) (#1024)
    * breaking out general interface
    
    * cleaning up exports
    
    * removing comments & references to specific interfaces
    
    * format
    
    * jsdoc comments
    
    * address pr comments
    
    * added param comment
    
    * addressing comments
    
    * Specific interface for provider enhancement (1/3) (#1021)
    
    * adding in app distro changes
    
    * removing comments & addings package refs
    
    * jsdoc comments
    
    * fix comments
    
    * adding periods to doc strings
    
    * fix wording
    
    * adding import/export statement
    
    * Specific interface for provider enhancement (2/3) (#1022)
    
    * adding in billing changes
    
    * removing comments & adding package refs
    
    * jsdoc comments
    
    * addressing pr comments
    
    * change handler doc string
    
    * changing to BillingEventHandler type
    
    * remove BillingEventHandler type
    
    * Specific interface for provider enhancement (3/3) (#1023)
    
    * adding in crashlytics changes
    
    * comments & adding package refs
    
    * address comments and make doc strings better
    
    * add opts.retry to event trigger

--- src/v2/providers/storage.ts
+++ src/v2/providers/storage.ts
@@ -359,7 +359,7 @@ export function onOperation(
           ...specificOpts?.labels,
         },
         eventTrigger: {
-          eventType: eventType,
+          eventType,
           eventFilters: {
             bucket,
           },
