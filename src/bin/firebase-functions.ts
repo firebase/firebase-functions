ff836a0bc01bf9726301de3dc3c1f449fd6d22c4
Thomas Bouldin <inlined@google.com>
2022-05-10 11:21:12 -0700

    Run formatter

--- src/bin/firebase-functions.ts
+++ src/bin/firebase-functions.ts
@@ -22,7 +22,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-
 import * as express from 'express';
 import { loadStack } from '../runtime/loader';
 
