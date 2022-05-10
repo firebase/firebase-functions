94ec7b49ef6436fa2fb881e21e0068f335ebcf42
Thomas Bouldin <inlined@google.com>
2022-05-10 11:35:26 -0700

    Fix copyright date

--- src/v2/providers/alerts/crashlytics.ts
+++ src/v2/providers/alerts/crashlytics.ts
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2017 Firebase
+// Copyright (c) 2022 Firebase
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
