3d504187b8eba21d5eddcd40fe5a2de873a04753
Daniel Young Lee <taeold@gmail.com>
2022-07-25 17:11:47 -0700

    Make all http functions private.

--- integration_test/functions/src/index.ts
+++ integration_test/functions/src/index.ts
@@ -178,6 +178,7 @@ export const integrationTests: any = functions
   .region(REGION)
   .runWith({
     timeoutSeconds: 540,
+    invoker: 'private',
   })
   .https.onRequest(async (req: Request, resp: Response) => {
     const testId = admin
