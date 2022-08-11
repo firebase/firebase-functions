108904307c802e71539978bdf2a1d93ce7ce29d2
Victor Fan <vsfan@google.com>
2022-08-11 16:04:55 -0700

    wip class-ification of expression type

--- src/v2/expressions.ts
+++ src/v2/expressions.ts
@@ -3,47 +3,94 @@
  * resolved to a value of the generic type parameter: i.e, you can pass
  * an Expression<number> as the value of an option that normally accepts numbers.
  */
-export type Expression<T extends string | number | boolean> = string; // eslint-disable-line
+abstract class Expression<T extends string | number | boolean> {
+  rawValue: string;
+
+  constructor(celLiteral: string) {
+    this.rawValue = celLiteral;
+  }
+
+  get val(): any {
+    return this.rawValue;
+  }
+
+  toString() {
+    return this.val();
+  }
+
+  toJSON() {
+    return this.toString();
+  }
+}
+
+export class ParamRef {
+  name: string;
+
+  constructor(name: string) {
+    if (!/^[A-Z_][A-Z0-9_]*$/.test(name)) {
+      throw new Error(
+        `Param name ${name} must start with an uppercase ASCII letter or underscore` +
+          ', and then consist of uppercase ASCII letters, digits, and underscores.'
+      );
+    }
+    this.name = name;
+  }
+}
+
+/**
+ * Creates an Expression that will cause an Options field to take on a runtime-defined
+ * value based on the value of a CF3 Param defined via the helpers in the v2/params
+ * namespace.
+ */
+export class ParamExpression<T extends string | number | boolean> extends Expression<T> {
+  constructor(param: ParamRef) {
+    super(`{{ params.${param.name} }}`);
+  }
+}
+
+/**
+ * Creates an Expression that will cause an Options field to be either true or false,
+ * based on a comparison between two values. The first value must be a CF3 Param, while
+ * the second can be another param or a literal value.
+ * @hidden
+ */
+export class BooleanExpression extends Expression<boolean> {
+  lhs: ParamRef;
+  rhs: ParamRef | string | number | boolean;
+  cmp: '==' | '<' | '>';
+
+  constructor(lhs:ParamRef, rhs: ParamRef | string | number | boolean, cmp?: '==' | '<' | '>') {
+    const rhsVal = (typeof rhs === "object") ? `params.${rhs.name}` : rhs
+    super(`{{ params.${lhs.name} ${cmp} ${rhsVal} }}`)
+  }
+}
+
+/**
+ * Creates an Expression that will cause an Options field to be one of two provided
+ * values based on a comparison from a BooleanExpression.
+ * @hidden
+ */
+export class TernaryExpression<T extends string | number | boolean> extends Expression<T> {
+  cond: BooleanExpression;
+  ifTrue: T | Expression<T>;
+  ifFalse: T | Expression<T>;
+
+  constructor(cond: BooleanExpression, ifTrue: T | Expression<T>, ifFalse: T | Expression<T>) {
+    const condVal = cond.toString().replace("{{ ", "").replace(" }}", "");
+    const trueVal = (typeof ifTrue === "object") ? ifTrue.toString() : ifTrue;
+    const falseVal = (typeof ifFalse === "object") ? ifFalse.toString() : ifFalse;
+    super(`{{ ${condVal} ? ${trueVal} : ${falseVal} }}`);
+  }
+}
+
+// nightmare Java hellscape
+//const foo:TernaryExpression<number> = new TernaryExpression(new BooleanExpression(new ParamRef('someSecret'), 0), 24, new ParamExpression(new ParamRef('myIntParam')));
+// {{ params.someSecret == 0 ? 24 : params.myIntParam }}
 
 export type Field<T extends string | number | boolean> =
   | T
   | Expression<T>
   | null;
-
-/**
- * Creates an Expression that will cause an Options field to take on a runtime-defined
- * value equal to a CF3 Param defined by the functions in v2/params.ts.
- */
-export function FromParam<T extends string | number | boolean>(
-  paramName: string
-): Expression<T> {
-  if (!/^[A-Z_][A-Z0-9_]*$/.test(paramName)) {
-    throw new Error(
-      `Param name ${paramName} must start with an uppercase ASCII letter or underscore` +
-        ', and then consist of uppercase ASCII letters, digits, and underscores.'
-    );
-  }
-  return `{{ params.${paramName} }}`;
-}
-
-/**
- * Creates an Expression that will cause an Options field to take on a runtime-defined
- * value obtained by evaluating the provided CEL expression.
- * The CLI does not currently have the capability to evaluate arbitrary CEL expressions.
- * You almost certainly want to use FromParam() instead.
- */
-export function FromCEL<T extends string | number | boolean>(
-  celString: string
-): Expression<T> {
-  const numOpens = celString.split('{{').length - 1;
-  const numCloses = celString.split('}}').length - 1;
-  if (numOpens == 0 || numOpens !== numCloses) {
-    throw new Error(
-      `Provided CEL expression ${celString} doesn't seem to be well formed.`
-    );
-  }
-  return celString;
-}
 
 /**
  * Casts an Expression to its literal string value, for use by __getSpec()
