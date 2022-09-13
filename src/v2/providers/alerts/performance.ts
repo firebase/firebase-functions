b9d2dd655e306dbbd8a83662192c8f286f495028
Cole Rogers <colerogers@google.com>
2022-09-13 15:46:45 -0400

    add alert type to index

--- src/v2/providers/alerts/performance.ts
+++ src/v2/providers/alerts/performance.ts
@@ -20,6 +20,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+/**
+ * Cloud functions to handle Firebase Performance Monitoring events from Firebase Alerts.
+ * @packageDocumentation
+ */
+
 import { CloudEvent, CloudFunction } from "../../core";
 import { EventHandlerOptions } from "../../options";
 import { FirebaseAlertData, getEndpointAnnotation } from ".";
@@ -29,28 +34,27 @@ import { FirebaseAlertData, getEndpointAnnotation } from ".";
  * Payload is wrapped inside a `FirebaseAlertData` object.
  */
 export interface ThresholdAlertPayload {
-  // Name of the trace or network request this alert is for (e.g. my_custom_trace, firebase.com/api/123)
+  /* Name of the trace or network request this alert is for (e.g. my_custom_trace, firebase.com/api/123) */
   eventName: string;
-  // The resource type this alert is for (i.e. trace, network request, screen rendering, etc.)
+  /* The resource type this alert is for (i.e. trace, network request, screen rendering, etc.) */
   eventType: string;
-  // The metric type this alert is for (i.e. success rate, response time, duration, etc.)
+  /* The metric type this alert is for (i.e. success rate, response time, duration, etc.) */
   metricType: string;
-  // The number of events checked for this alert condition
+  /* The number of events checked for this alert condition */
   numSamples: number;
-  // The threshold value of the alert condition without units (e.g. "75", "2.1")
+  /* The threshold value of the alert condition without units (e.g. "75", "2.1") */
   thresholdValue: number;
-  // The unit for the alert threshold (e.g. "percent", "seconds")
+  /* The unit for the alert threshold (e.g. "percent", "seconds") */
   thresholdUnit: string;
-  // The percentile of the alert condition, can be 0 if percentile is not
-  // applicable to the alert condition; range: [0, 100]
+  /* The percentile of the alert condition, can be 0 if percentile is not applicable to the alert condition; range: [0, 100] */
   conditionPercentile: number;
-  // The app version this alert was triggered for, can be empty if the alert is for a network request (because the alert was checked against data from all versions of app) or a web app (where the app is versionless)
+  /* The app version this alert was triggered for, can be empty if the alert is for a network request (because the alert was checked against data from all versions of app) or a web app (where the app is versionless) */
   appVersion: string;
-  // The value that violated the alert condition (e.g. "76.5", "3")
+  /* The value that violated the alert condition (e.g. "76.5", "3") */
   violationValue: number;
-  // The unit for the violation value (e.g. "percent", "seconds")
+  /* The unit for the violation value (e.g. "percent", "seconds") */
   violationUnit: string;
-  // The link to Fireconsole to investigate more into this alert
+  /* The link to Fireconsole to investigate more into this alert */
   investigateUri: string;
 }
 
@@ -76,12 +80,23 @@ export interface PerformanceOptions extends EventHandlerOptions {
   appId?: string;
 }
 
+/**
+ * Declares a function that can handle receiving performance threshold alerts.
+ * @param handler - Event handler which is run every time a threshold alert is received.
+ * @returns A function that you can export and deploy.
+ */
 export function onThresholdAlertPublished(
   handler: (
     event: PerformanceEvent<ThresholdAlertPayload>
   ) => any | Promise<any>
 ): CloudFunction<PerformanceEvent<ThresholdAlertPayload>>;
 
+/**
+ * Declares a function that can handle receiving performance threshold alerts.
+ * @param appId - A specific application the handler will trigger on.
+ * @param handler - Event handler which is run every time a threshold alert is received.
+ * @returns A function that you can export and deploy.
+ */
 export function onThresholdAlertPublished(
   appId: string,
   handler: (
@@ -89,6 +104,12 @@ export function onThresholdAlertPublished(
   ) => any | Promise<any>
 ): CloudFunction<PerformanceEvent<ThresholdAlertPayload>>;
 
+/**
+ * Declares a function that can handle receiving performance threshold alerts.
+ * @param opts - Options that can be set on the function.
+ * @param handler - Event handler which is run every time a threshold alert is received.
+ * @returns A function that you can export and deploy.
+ */
 export function onThresholdAlertPublished(
   opts: PerformanceOptions,
   handler: (
@@ -96,6 +117,12 @@ export function onThresholdAlertPublished(
   ) => any | Promise<any>
 ): CloudFunction<PerformanceEvent<ThresholdAlertPayload>>;
 
+/**
+ * Declares a function that can handle receiving performance threshold alerts.
+ * @param appIdOrOptsOrHandler - A specific application, options, or an event-handling function.
+ * @param handler - Event handler which is run every time a threshold alert is received.
+ * @returns A function that you can export and deploy.
+ */
 export function onThresholdAlertPublished(
   appIdOrOptsOrHandler:
     | string
