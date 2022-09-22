3fa2c94d2f2395572ed618eec34663914baa0225
Cole Rogers <colerogers@google.com>
2022-09-22 15:23:59 -0700

    adding in tests & fixing remote config tsdoc strings

--- src/v2/providers/remoteConfig.ts
+++ src/v2/providers/remoteConfig.ts
@@ -27,61 +27,61 @@ import { EventHandlerOptions, getGlobalOptions, optionsToEndpoint } from "../opt
 /** @internal */
 export const eventType = "google.firebase.remoteconfig.remoteConfig.v1.updated";
 
-/* All the fields associated with the person/service account that wrote a Remote Config template. */
+/** All the fields associated with the person/service account that wrote a Remote Config template. */
 export interface ConfigUser {
-  /* Display name. */
+  /** Display name. */
   name: string;
 
-  /* Email address. */
+  /** Email address. */
   email: string;
 
-  /* Image URL. */
+  /** Image URL. */
   imageUrl: string;
 }
 
-/* What type of update was associated with the Remote Config template version. */
+/** What type of update was associated with the Remote Config template version. */
 export type ConfigUpdateOrigin =
-  /* Catch-all for unrecognized values. */
+  /** Catch-all for unrecognized values. */
   | "REMOTE_CONFIG_UPDATE_ORIGIN_UNSPECIFIED"
-  /* The update came from the Firebase UI. */
+  /** The update came from the Firebase UI. */
   | "CONSOLE"
-  /* The update came from the Remote Config REST API. */
+  /** The update came from the Remote Config REST API. */
   | "REST_API"
-  /* The update came from the Firebase Admin Node SDK. */
+  /** The update came from the Firebase Admin Node SDK. */
   | "ADMIN_SDK_NODE";
 
-/* Where the Remote Config update action originated. */
+/** Where the Remote Config update action originated. */
 export type ConfigUpdateType =
-  /* Catch-all for unrecognized enum values */
+  /** Catch-all for unrecognized enum values */
   | "REMOTE_CONFIG_UPDATE_TYPE_UNSPECIFIED"
-  /* A regular incremental update */
+  /** A regular incremental update */
   | "INCREMENTAL_UPDATE"
-  /* A forced update. The ETag was specified as "*" in an UpdateRemoteConfigRequest request or the "Force Update" button was pressed on the console */
+  /** A forced update. The ETag was specified as "*" in an UpdateRemoteConfigRequest request or the "Force Update" button was pressed on the console */
   | "FORCED_UPDATE"
-  /* A rollback to a previous Remote Config template */
+  /** A rollback to a previous Remote Config template */
   | "ROLLBACK";
 
-/* The data within Firebase Remote Config update events. */
+/** The data within Firebase Remote Config update events. */
 export interface ConfigUpdateData {
-  /* The version number of the version's corresponding Remote Config template. */
+  /** The version number of the version's corresponding Remote Config template. */
   versionNumber: number;
 
-  /* When the Remote Config template was written to the Remote Config server. */
+  /** When the Remote Config template was written to the Remote Config server. */
   updateTime: string;
 
-  /* Aggregation of all metadata fields about the account that performed the update. */
+  /** Aggregation of all metadata fields about the account that performed the update. */
   updateUser: ConfigUser;
 
-  /* The user-provided description of the corresponding Remote Config template. */
+  /** The user-provided description of the corresponding Remote Config template. */
   description: string;
 
-  /* Where the update action originated. */
+  /** Where the update action originated. */
   updateOrigin: ConfigUpdateOrigin;
 
-  /* What type of update was made. */
+  /** What type of update was made. */
   updateType: ConfigUpdateType;
 
-  /* Only present if this version is the result of a rollback, and will be the version number of the Remote Config template that was rolled-back to. */
+  /** Only present if this version is the result of a rollback, and will be the version number of the Remote Config template that was rolled-back to. */
   rollbackSource: number;
 }
 
