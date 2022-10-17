1d45023735fe7f89ae53cff7a9ebc66bc499f112
Daniel Young Lee <taeold@gmail.com>
2022-10-17 11:41:02 -0400

    Wrap trace context for v2 callable functions.

--- src/v2/providers/https.ts
+++ src/v2/providers/https.ts
@@ -293,12 +293,14 @@ export function onCall<T = any, Return = any | Promise<any>>(
   // onCallHandler sniffs the function length to determine which API to present.
   // fix the length to prevent api versions from being mismatched.
   const fixedLen = (req: CallableRequest<T>) => handler(req);
-  const func: any = onCallHandler(
-    {
-      cors: { origin, methods: "POST" },
-      enforceAppCheck: opts.enforceAppCheck ?? options.getGlobalOptions().enforceAppCheck,
-    },
-    fixedLen
+  const func: any = wrapTraceContext(
+    onCallHandler(
+      {
+        cors: { origin, methods: "POST" },
+        enforceAppCheck: opts.enforceAppCheck ?? options.getGlobalOptions().enforceAppCheck,
+      },
+      fixedLen
+    )
   );
 
   const baseOpts = options.optionsToEndpoint(options.getGlobalOptions());
