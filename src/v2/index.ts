049327118394483f474841db42da2bd7acfe67cc
Daniel Lee <danielylee@google.com>
2024-09-09 21:21:26 -0700

    Expose functions.config in the v2 namespace to avoid breaking the Functions Emulator. (#1607)

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -73,3 +73,7 @@ export { Change } from "../common/change";
 // NOTE: Equivalent to `export * as params from "../params"` but api-extractor doesn't support that syntax.
 import * as params from "../params";
 export { params };
+
+// NOTE: Required to support the Functions Emulator which monkey patches `functions.config()`
+// TODO(danielylee): Remove in next major release.
+export { config } from "../v1/config";
