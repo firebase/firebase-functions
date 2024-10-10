0532b2f015dd1533651aef52cad4ef11a88f5a27
exaby73 <nabeelparkar99@gmail.com>
2024-10-10 17:10:49 +0530

    remove(integration_test): Storage V1 delete tests

--- integration_test/functions/src/v1/storage-tests.ts
+++ integration_test/functions/src/v1/storage-tests.ts
@@ -3,6 +3,7 @@ import * as functions from "firebase-functions";
 import { REGION } from "../region";
 import { sanitizeData } from "../utils";
 
+// TODO: (b/372315689) Re-enable function once bug is fixed
 // export const storageOnDeleteTests: any = functions
 //   .runWith({
 //     timeoutSeconds: 540,
@@ -24,26 +25,26 @@ import { sanitizeData } from "../utils";
 //       .set(sanitizeData(context));
 //   });
 
-// export const storageOnFinalizeTests: any = functions
-//   .runWith({
-//     timeoutSeconds: 540,
-//   })
-//   .region(REGION)
-//   .storage.bucket()
-//   .object()
-//   .onFinalize(async (object, context) => {
-//     const testId = object.name?.split(".")[0];
-//     if (!testId) {
-//       functions.logger.error("TestId not found for storage object finalize");
-//       return;
-//     }
-//
-//     await admin
-//       .firestore()
-//       .collection("storageOnFinalizeTests")
-//       .doc(testId)
-//       .set(sanitizeData(context));
-//   });
+export const storageOnFinalizeTests: any = functions
+  .runWith({
+    timeoutSeconds: 540,
+  })
+  .region(REGION)
+  .storage.bucket()
+  .object()
+  .onFinalize(async (object, context) => {
+    const testId = object.name?.split(".")[0];
+    if (!testId) {
+      functions.logger.error("TestId not found for storage object finalize");
+      return;
+    }
+
+    await admin
+      .firestore()
+      .collection("storageOnFinalizeTests")
+      .doc(testId)
+      .set(sanitizeData(context));
+  });
 
 export const storageOnMetadataUpdateTests: any = functions
   .runWith({
