d1809411c4a8135d6520c5429d7b83fe8e53eddf
Darren Ackers <ackers86@hotmail.com>
2025-10-21 13:09:47 +0100

    fix(*): updated ts config to fix import error

--- spec/fixtures/http.ts
+++ spec/fixtures/http.ts
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import * as nock from 'nock';
+import nock from "nock";
 
 interface AccessToken {
   access_token: string;
