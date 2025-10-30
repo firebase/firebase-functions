337b47d8d06d6c38ed601edc65d166aab067fca7
Daniel Lee <danielylee@google.com>
2025-10-30 09:54:50 -0700

    BREAKING: Break TS build that uses functions.config() (#1757)
    Type out `functions.config()` API. Use of `functions.config()` API should now cause a BUILD error at compile time.
    
    ```ts
    import * as functions from "firebase-functions";
    import {onRequest} from "firebase-functions/https";
    import * as logger from "firebase-functions/logger";
    
    const cfg = functions.config()
    
    export const helloWorld = onRequest((request, response) => {
      logger.info("Hello logs!", cfg);
      response.send("Hello from Firebase!");
    });
    ```
    ```bash
    $ npm run build
    
    > build
    > tsc
    
    src/index.ts:5:23 - error TS2349: This expression is not callable.
      Type 'never' has no call signatures.
    
    5 const cfg = functions.config()
                            ~~~~~~
    
    
    Found 1 error in src/index.ts:5
    ```

--- src/v1/config.ts
+++ src/v1/config.ts
@@ -27,10 +27,10 @@ export { firebaseConfig } from "../common/config";
  * Migrate to environment parameters using the `params` module immediately.
  * Migration guide: https://firebase.google.com/docs/functions/config-env#migrate-config
  */
-export function config(): Record<string, any> {
+export const config: never = (() => {
   throw new Error(
     "functions.config() has been removed in firebase-functions v7. " +
       "Migrate to environment parameters using the params module. " +
       "Migration guide: https://firebase.google.com/docs/functions/config-env#migrate-config"
   );
-}
+}) as never;
