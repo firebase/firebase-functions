3577d9ba077c3f39533a7ac0f6fcf71154108699
Daniel Lee <danielylee@google.com>
2025-12-09 23:31:46 -0500

    feat: remove attemptDeadlineSeconds in v2 scheduled functions (#1776)
    * feat: tie scheduled function attemptDeadline to timeoutSeconds
    
    * feat: tie scheduled function attemptDeadline to timeoutSeconds
    
    * run formatter.
    
    * run formatter.
    
    * nit: fix formatting

--- spec/runtime/manifest.spec.ts
+++ spec/runtime/manifest.spec.ts
@@ -286,7 +286,6 @@ describe("initScheduleTrigger", () => {
     expect(st).to.deep.eq({
       schedule: "every 30 minutes",
       timeZone: RESET_VALUE,
-      attemptDeadlineSeconds: RESET_VALUE,
       retryConfig: {
         retryCount: RESET_VALUE,
         maxDoublings: RESET_VALUE,
