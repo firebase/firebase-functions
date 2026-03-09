1e86ac7b9843c42269866b9b32d0fd97c9bd4304
Varun Shetty <shettyvarun@google.com>
2026-03-09 16:08:07 +0000

    Fix missing FDC docs using docgen index workaround

---
+++ src/v2/index.doc.ts
@@ -0,0 +1,8 @@
+/**
+ * Documentation-only entry point.
+ * DO NOT use for runtime imports.
+ */
+export * from "./index";
+
+// Explicitly export the hidden graphql module for documentation generation
+export * as graphql from "./providers/dataconnect/graphql";
